640916c6b6074f5fbfae469dba35d4f8153a2691
Sachinvb <sachinvb27@gmail.com>
2024-09-06 10:43:48 +0530

    first commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of Axios with default settings
 const api = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'https://waste-disposal-v1cm.onrender.com/',
   // baseURL: 'https://devtest-7wh6.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
